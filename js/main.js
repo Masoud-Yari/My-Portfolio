16bfc4ead9271b9510a4b4fc83df5fc2365c1951
Masoud-Yari <masoudd.yarii@gmail.com>
2020-09-13 23:21:22 +0430

    add smooth scroll and fix navbar

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const navList = document.querySelector('.nav-list');
 
+
 hamburgerIcon.addEventListener('click', () => {
     if(navList.style.top !== '0vh'){
-        
+        // show close icon
+        hamburgerIcon.classList.add('close-icon');
+
         navList.style.top = '0vh';
     }else{
+        // remove close icon
+        hamburgerIcon.classList.remove('close-icon');
+        
         navList.style.top = '-30vh';
     }
 });
@@ -16,4 +22,9 @@ hamburgerIcon.addEventListener('click', () => {
 // AOS Config
 AOS.init({
     duration: 500
+});
+
+// Smooth scroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 500
 });
